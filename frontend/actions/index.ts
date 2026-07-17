8134be927953d52bed1782f2fb08c69533f9a147
CristhianZ2022 <cris12x12@gmail.com>
2026-07-16 23:03:58 -0500

    Separate server actions from the book to improve layer separation and add mutations

--- frontend/actions/index.ts
+++ frontend/actions/index.ts
@@ -1,9 +1,21 @@
 import { changePasswordAction, loginUserAction, registerUserAction } from "./auth";
+import {
+  createClientAction,
+  updateClientAction,
+  uploadFileAction,
+  createPlanAction,
+} from "./mutations";
 
 export const actions = {
   auth: {
     registerUserAction,
     loginUserAction,
     changePasswordAction,
-  }
-} 
+  },
+  mutations: {
+    createClientAction,
+    updateClientAction,
+    uploadFileAction,
+    createPlanAction,
+  },
+};
