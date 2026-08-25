0b252078a3ddd4aae5a583591ece5709a51cfb7c
CristhianZ2022 <cris12x12@gmail.com>
2026-08-24 21:14:27 -0500

    fix: It was added upload block for the parameters new the images

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,4 +1,24 @@
 export default () => ({
+  upload: {
+    config: {
+      sizeLimit: 5 * 1024 * 1024,
+      breakpoints: {
+        thumbnail: { width: 160, height: 160 },
+        small: { width: 500, height: 500 },
+        medium: { width: 1000, height: 1000 },
+        large: { width: 1400 },
+      },
+      sharp: {
+        cache: true,
+        concurrency: 4,
+      },
+      security: {
+        allowedTypes: ['image/*', 'application/pdf'],
+        deniedTypes: ['application/x-sh', 'application/x-dosexec'],
+      },
+      concurrentUploadSize: 5,
+    },
+  },
   'users-permissions': {
     config: {
       jwtManagement: 'refresh', // Para habilitar endpoints de gestión de JWT
